f3b6a7ac7b0f034fead14746cf2b5a42495415b0
KangDroid <hyunwoo286@gmail.com>
2020-07-10 12:16:22 +0900

    Executor: 3+3 works

--- mpipe_src/MainExecuter.h
+++ mpipe_src/MainExecuter.h
@@ -13,9 +13,13 @@ private:
     string to_execute = "00000011101000101001100100100101";
 public:
     void init() {
+        int t_val = 3;
+        for (int i = 0; i < 32; i++) {
+            regmem_unit.set_actual_dec(i, t_val);
+        }
         // Parse from I-Mem
         imem_unit.seperate(to_execute);
-        register_unit.initRegister(imem_unit.get_rs(), imem_unit.get_rt(), imem_unit.get_rd());
+        register_unit.initRegister(regmem_unit, imem_unit.get_rs(), imem_unit.get_rt(), imem_unit.get_rd());
         alu_unit.ALU_operation(register_unit.get_rs_forward(), register_unit.get_rt_forward());
         bool* tmp = alu_unit.get_ALU_result();
 
