8eeb1e7072162914da0b025c81933ff92855d82a
KangDroid <hyunwoo286@gmail.com>
2020-07-10 10:37:04 +0900

    Add some default unit

--- mpipe_src/MainExecuter.h
+++ mpipe_src/MainExecuter.h
@@ -1,2 +1,14 @@
+#include "ALU.h"
+#include "Instruction_Memory.h"
+#include "RMem.h"
+#include "Register.h"
+#include "DMem.h"
 class MainExecuter {
+private:
+    ALU alu_unit;
+    Instruction_Memory imem_unit;
+    RMem regmem_unit;
+    Register regsiter_unit;
+    DMem datamem_unit;
+public:
 };
