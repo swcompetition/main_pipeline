5bddc0f9e1511308a86fd9a08017313d0a9614fc
KangDroid <hyunwoo286@gmail.com>
2020-07-10 12:43:36 +0900

    Change some test case to accurate one

--- mpipe_src/MainExecuter.h
+++ mpipe_src/MainExecuter.h
@@ -13,10 +13,13 @@ private:
     string to_execute = "00000011101000101001100100100101";
 public:
     void init() {
-        int t_val = 3;
-        for (int i = 0; i < 32; i++) {
-            regmem_unit.set_actual_dec(i, t_val);
-        }
+        int rs_idx = 29;
+        int rt_idx = 2;
+        regmem_unit.set_actual_dec(rs_idx, 10);
+        regmem_unit.set_actual_dec(rt_idx, 20);
+        // for (int i = 0; i < 32; i++) {
+        //     regmem_unit.set_actual_dec(i, t_val);
+        // }
         // Parse from I-Mem
         imem_unit.seperate(to_execute);
 
@@ -32,6 +35,6 @@ public:
         register_unit.setRDValue(alu_unit.get_ALU_result());
 
         // Check output
-        cout << register_unit.getRDValue() << endl;
+        cout << "Final RD Value: " << register_unit.getRDValue() << endl;
     }
 };
